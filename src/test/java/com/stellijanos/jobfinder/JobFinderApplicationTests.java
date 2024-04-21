2d030649c2d302be4e02762413b8b899aa750545
Janos Stelli <stellijanos23@gmail.com>
2024-04-21 19:09:50 +0300

    Initial commit

---
+++ src/test/java/com/stellijanos/jobfinder/JobFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stellijanos.jobfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
